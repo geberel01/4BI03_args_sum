0dbbfbf5624577a00a1d8abef3863de5ff37eb21
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-11 11:20:50 +0000

    ok

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 //ESERCIZIO:
 //DARE IN INGRESSO STRINGHE NUMERICHE
 //E STAMPARE LA SOMMA DI TUTTE
-
+#include <string.h>
 #include <stdio.h>
 int i;
+int sum=0;
+char str[100][20];
 int main(int argc, char *argv[]) {
   for(i=0;i<argc;i++)
-    printf("Pos %d\t stringa: %s\n",i,argv[i]);
-  return 0;
-}
+    {
+      printf("Pos %d\t stringa: %s\n",i,argv[i]);
+      strcpy(str,argv[i]);
+      sum += atoi(str);
+    }
+
